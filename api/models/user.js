7778216d4d4144f91a49a46fbd9ca9a2e6892a05
Karla Rangel <krangelhdz@gmail.com>
2024-08-13 11:02:37 +0100

    modified user model

--- api/models/user.js
+++ api/models/user.js
@@ -1,8 +1,11 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
+  full_name: { type: String, required: true },
+  user_name: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
+  favourites: { type: Array, required: false },
 });
 
 const User = mongoose.model("User", UserSchema);
