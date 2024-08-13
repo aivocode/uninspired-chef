287e1ed5361d39fdd4a784f2b99aa463da1ff529
QS-Coding <169077428+QS-Coding@users.noreply.github.com>
2024-08-13 13:32:42 +0100

    Added pages routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,8 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
 import "./App.css";
 import { HomePage } from "./pages/Home/HomePage";
-import { LoginPage } from "./pages/Login/LoginPage";
-import { SignupPage } from "./pages/Signup/SignupPage";
-import { FeedPage } from "./pages/Feed/FeedPage";
+import { AuthenticationPage } from "./pages/AuthenticationPage";
+import { PantryPage } from "./pages/PantryPage";
 
 // docs: https://reactrouter.com/en/main/start/overview
 const router = createBrowserRouter([
@@ -13,16 +11,12 @@ const router = createBrowserRouter([
     element: <HomePage />,
   },
   {
-    path: "/login",
-    element: <LoginPage />,
+    path: "/auth",
+    element: <AuthenticationPage />,
   },
   {
-    path: "/signup",
-    element: <SignupPage />,
-  },
-  {
-    path: "/posts",
-    element: <FeedPage />,
+    path: "/pantry",
+    element: <PantryPage />,
   },
 ]);
 
