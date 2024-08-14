1e46cfa8fdbcf5f7baef6b829a91b33aeebe3541
chobs95 <charlieroberts201@hotmail.co.uk>
2024-08-14 15:35:13 +0100

    fixed broken feedpage on login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,34 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-// import "../tailwind.css";
-// import './global.css';
-// import "./index.css";
-// import { HomePage } from "./pages/HomePage";
-// import { AuthenticationPage } from "./pages/AuthenticationPage";
+
+import "./App.css";
+import { HomePage } from "./pages/Home/HomePage";
+import { LoginPage } from "./pages/Login/LoginPage";
+import { SignupPage } from "./pages/Signup/SignupPage";
+import { FeedPage } from "./pages/Feed/FeedPage";
 import { PantryPage } from "./pages/PantryPage";
 
 // docs: https://reactrouter.com/en/main/start/overview
 const router = createBrowserRouter([
-  // {
-  //   path: "/",
-  //   element: <HomePage />,
-  // },
-  // {
-  //   path: "/auth",
-  //   element: <AuthenticationPage />,
-  // },
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/signup",
+    element: <SignupPage />,
+  },
+  {
+    path: "/posts",
+    element: <FeedPage />,
+  },
   {
     path: "/pantry",
-    element: <PantryPage />,
-  },
+    element: <PantryPage/>
+  }
 ]);
 
 const App = () => {
