e9cc1e84de690979bc31f1ecd98519641d26fa13
QS-Coding <169077428+QS-Coding@users.noreply.github.com>
2024-08-14 15:18:47 +0100

    Removed legacy import that was no longer needed

--- frontend/src/components/RecipeCard/RecipeCard.jsx
+++ frontend/src/components/RecipeCard/RecipeCard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { FullRecipePopout } from '../FullRecipePopout/FullRecipePopout';
 
 export const RecipeCard = ({ title, description, saved }) => {
