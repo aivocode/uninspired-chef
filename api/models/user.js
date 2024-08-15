c2c464f10dc64b53ad0b0094c6ebb6debc0aeba7
Tara <taconlin@hotmail.co.uk>
2024-08-15 16:03:24 +0100

    updated favourite recipes field

--- api/models/user.js
+++ api/models/user.js
@@ -1,8 +1,13 @@
 const mongoose = require("mongoose");
+const { RecipeSchema } = require('./recipe'); //importing the recipe schema
 
+// Fields to be in the database
 const UserSchema = new mongoose.Schema({
+  fullName: { type: String, required: true },
+  userName: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
+  favouritedRecipes: [RecipeSchema], //array of Recipe subdocuments (i.e. recipe objects from Edamam API that we've saved)
 });
 
 const User = mongoose.model("User", UserSchema);
