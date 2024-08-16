3a4fc0a877cd054e9c5d6047291d7dca86545801
Aivaras Voleika <154103843+aivocode@users.noreply.github.com>
2024-08-16 15:33:49 +0100

    Further styling with Tailwind classes & custom CSS, removal of redudant states

--- frontend/src/components/Pantry/Pantry.jsx
+++ frontend/src/components/Pantry/Pantry.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import { AddIngredient } from "../AddIngredientPopout/AddIngredientPopout";
-import "../../static/PantryPage.css";
 import { createPantry, getPantry, updatePantry } from "../../services/pantry";
 
 export const Pantry = ({ className }) => {
@@ -13,16 +12,10 @@ export const Pantry = ({ className }) => {
 
   const [showPopout, setShowPopout] = useState(false);
 
-  // we add ingredients from popup here, so it can be passed to display on page before passing to createPantry
-  const [ingredientsArray, setIngredientsArray] = useState([]);
-  // here we store pantryId that can be reused between renders
-  const [pantryId, setPantryId] = useState("");
-  // here we store all our Pantry objects within array, so we can render them in return()
-  const [pantryObjectsArray, setPantryObjectsArray] = useState([]);
-  // here we store render mode, 0 for create Pantry form, 1 for show Pantry contents, 2 for edit Pantry form
-  const [pantryRenderMode, setPantryRenderMode] = useState("");
-  // here we store message that comes from backend, so we can render it later in our conditional statements in return()
-  const [message, setMessage] = useState("");
+  const [pantryRenderMode, setPantryRenderMode] = useState(""); // here we store render mode, 0 for create Pantry form, 1 for show Pantry contents, 2 for edit Pantry form
+  const [ingredientsArrayState, setIngredientsArrayState] = useState([]); // we add ingredients from popup here, so it can be passed to display on Create Pantry mode, before passing to createPantry
+  const [dataState, setDataState] = useState({}); // here we store data object that comes from backend response
+  const [message, setMessage] = useState(""); // here we store message that comes from backend, so we can render it later in our conditional statements in return()
 
   // here we store our ingredients string, so validator can later check if it's format is correct
   // and display message if it is not
@@ -35,30 +28,30 @@ export const Pantry = ({ className }) => {
     }
   }, []);
 
-  console.log(pantryRenderMode);
+  // console.log(pantryRenderMode);
+  console.log(ingredientsArrayState);
+  // console.log(dataState);
+  // console.log(message);
+
   const addToIngredientsArray = (ingredientString) => {
-    setIngredientsArray([...ingredientsArray, ingredientString]);
+    setIngredientsArrayState([...ingredientsArrayState, ingredientString]); // we add new array item each time submit from popup
   };
-  // console.log(ingredientsArray);
 
   const fetchGetPantry = async () => {
     const data = await getPantry(userId);
-    console.log(data);
     if (data.status === 400) {
-      // first time we login, we have no Pantry, which will have status code 400, so render mode is set to 0
-      setPantryRenderMode(0);
+      setPantryRenderMode(0); // first time we login, we have no Pantry, which will have status code 400, so render mode is set to 0
       setMessage(data.message);
     } else if ((data.status = 200)) {
-      setPantryId(data.pantryId);
-      setPantryObjectsArray(data.ingredientsArray);
-      setPantryRenderMode(1);
+      setPantryRenderMode(1); // When we already have Pantry, which will have status code 200, so render mode is set to 1
+      setDataState(data);
     }
   };
 
   const fetchCreatePantry = async () => {
-    const data = await createPantry(token, userId, ingredientsArray);
-    fetchGetPantry();
+    const data = await createPantry(token, userId, ingredientsArrayState);
     setMessage(data.message);
+    fetchGetPantry();
   };
 
   const handleAddButtonClick = () => {
@@ -74,82 +67,83 @@ export const Pantry = ({ className }) => {
       <div className="relative pantry-content">
         <div className="pantry-background" />
         <div className="pantry-header-bar" />
-        <div className="pantry-content-box">
-          <div className="pantry-content-inner">
-            <img
-              className="line line-top"
-              alt="Line"
-              src="https://c.animaapp.com/2O5FotJ7/img/line-2-1.svg"
-            />
-            <img
-              className="line line-bottom"
-              alt="Line"
-              src="https://c.animaapp.com/2O5FotJ7/img/line-3-1.svg"
-            />
+        <div className="pantry-content-box p-10">
+          {/* if logged in and render mode is set to create Pantry mode 0, render these elements */}
+          {token && pantryRenderMode === 0 && (
+            <>
+              <div className="pantry-title flex flex-col text-center mb-10">
+                CREATE PANTRY
+              </div>
 
-            {/* if logged in and render mode is set to create Pantry mode 0, render these elements */}
-            {token && pantryRenderMode === 0 && (
-              <>
-                <div className="pantry-title">CREATE PANTRY</div>
-                <div className="pantry-ingredients">
+              <div className="flex flex-col">
+                <div>
                   {/* shows message if it exists */}
-                  {message && (
-                    <>
-                      <div>{message}</div>
-                      <br />
-                    </>
-                  )}
+                  {message && <div className="mb-2 text-center">{message}</div>}
+                </div>
 
-                  <div>Ingredients:</div>
-                  <br />
-                  {ingredientsArray.map((element, index) => (
-                    <div key={index}>
-                      <div>{element}</div>
+                <button className="add-button" onClick={handleAddButtonClick}>
+                  <div className="add-button-text">➕ADD</div>
+                </button>
+
+                <div className="container m-auto grid grid-cols-6">
+                  {ingredientsArrayState.map((element, index) => (
+                    <div className="flex items-end mb-2" key={index}>
+                      <div className="flex items-end badge-ingredient">
+                        <div>{element}</div>
+                      </div>
                     </div>
                   ))}
-                  <br />
-                  {/* render button only there some ingredients added to ingredientsArray */}
-                  {ingredientsArray.length > 0 && (
-                    <button className="add-button" onClick={fetchCreatePantry}>
-                      <div className="add-button-text">🧺CREATE</div>
-                    </button>
-                  )}
                 </div>
 
-                <div className="add-button-container">
-                  <button className="add-button" onClick={handleAddButtonClick}>
-                    <div className="add-button-text">🍅ADD</div>
+                {/* render button only there some ingredients added to ingredientsArray */}
+                {ingredientsArrayState.length > 0 && (
+                  <button className="add-button" onClick={fetchCreatePantry}>
+                    <div className="add-button-text">CREATE</div>
                   </button>
-                </div>
-              </>
-            )}
+                )}
+              </div>
+            </>
+          )}
 
-            {/* if logged in and render mode is set to view Pantry mode 1, render these elements */}
-            {token && pantryRenderMode === 1 && (
-              <>
-                <div className="pantry-title">PANTRY</div>
-                <div className="pantry-ingredients">
-                  <div>
-                    {/* shows message if it exists */}
-                    {message && <div>{message}</div>}
-                  </div>
-                  <br />
-                  {pantryObjectsArray.map((element, index) => (
-                    <div key={index}>
-                      <div>{element.label}</div>
+          {/* if logged in and render mode is set to view Pantry mode 1, render these elements */}
+          {token && pantryRenderMode === 1 && (
+            <>
+              <div className="pantry-title flex flex-col text-center mb-10">
+                PANTRY
+              </div>
+
+              <div className="flex flex-col">
+                <div>
+                  {/* shows message if it exists */}
+                  {message && <div className="mb-2 text-center">{message}</div>}
+                </div>
+
+                <div className="container m-auto grid grid-cols-3">
+                  {dataState.ingredientsArray.map((element, index) => (
+                    <div className="flex items-end mb-2" key={index}>
+                      <button className="mr-2 pt-1 pb-1" onClick={console.log("Clicked")}>
+                        ✖️
+                      </button>
+                      <button
+                        type="button"
+                        className="mr-2 pt-1 pb-1"
+                        onClick={handleAddButtonClick}
+                      >
+                        &#9998;
+                      </button>
+                      <div className="flex items-end badge-ingredient">
+                        <img src={element.image} class="avatar-ingredient mr-1"/>
+                        <div>{element.label}</div>
+                      </div>
                     </div>
                   ))}
-                  <br />
                 </div>
-
-                <div className="add-button-container">
-                  <button className="add-button" onClick={handleAddButtonClick}>
-                    <div className="add-button-text">✏️EDIT</div>
-                  </button>
-                </div>
-              </>
-            )}
-          </div>
+                <button className="add-button" onClick={fetchCreatePantry}>
+                  UPDATE
+                </button>
+              </div>
+            </>
+          )}
         </div>
         <div className="white-box" />
         <div className="header-bar" />
