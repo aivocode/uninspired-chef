3d2f7533371e3d48db02ab0eaff9fc0eb26e280c
AlexHaigh <alexandra.haigh@outlook.com>
2024-08-20 11:31:11 +0100

    paragraph addition

--- frontend/src/components/ExampleText/ExampleText.jsx
+++ frontend/src/components/ExampleText/ExampleText.jsx
@@ -1,14 +1,24 @@
 export const ExampleText = ({ recipe }) => {
   const healthLabels = Object.values(recipe.healthLabels);
-  //   const dietLabels = Object.values(recipe.dietLabels);
+  const dietLabels = Object.values(recipe.dietLabels);
   //   console.log(healthLabels);
   const healthLabelstext = healthLabels.join(", ") + ".";
-  //   const dietLabelsText = dietLabels.join(", ") + ".";
+  //const dietLabelsText = dietLabels.join(", ") + ".";
+
+  const myDietList = dietLabels.map((item, index) => (
+    <li key={index}>{item}</li>
+  ));
 
   return (
     <div>
+      <ul>{myDietList}</ul>
+      Health Labels
       <p>{healthLabelstext}</p>
-      {/* <p>{dietLabelsText}</p> */}
+      <ul>
+        <li>Coffee</li>
+        <li>Tea</li>
+        <li>Milk</li>
+      </ul>
     </div>
   );
 };
