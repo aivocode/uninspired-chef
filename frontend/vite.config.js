9f18ba64d6ae42c7119deeabefc2825b2f8099d8
QS-Coding <169077428+QS-Coding@users.noreply.github.com>
2024-08-21 12:56:23 +0100

    Vite.config changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,10 +4,6 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',  // Allows access from network
-    port: parseInt(import.meta.env.VITE_FRONTEND_PORT || '5173', 10),  // Use the environment variable or fallback to 5173
-  },
   test: {
     globals: true,
     environment: "jsdom",
