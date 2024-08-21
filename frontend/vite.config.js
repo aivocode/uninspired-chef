937a13f40b008e049b93f7fd1a21735308752152
QS-Coding <169077428+QS-Coding@users.noreply.github.com>
2024-08-21 10:13:54 +0100

    Further vite.config changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',  // Allows access from network
-    port: parseInt(import.meta.env.VITE_FRONTEND_PORT) || 5173,  // Use the environment variable or fallback to 5173
+    port: parseInt(import.meta.env.VITE_FRONTEND_PORT || '5173', 10),  // Use the environment variable or fallback to 5173
   },
   test: {
     globals: true,
